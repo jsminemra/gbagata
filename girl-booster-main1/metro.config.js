73423bbe0878e2aa62b47565298fe0981878683d
jsminemra <jasminemariaar6@gmail.com>
2025-08-02 16:40:46 -0300

    Update metro

--- girl-booster-main1/metro.config.js
+++ girl-booster-main1/metro.config.js
@@ -1,3 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const config = getDefaultConfig(__dirname);
 
-module.exports = getDefaultConfig(__dirname);
+// Adicionar suporte para mais tipos de assets
+config.resolver.assetExts.push('png', 'jpg', 'jpeg', 'gif', 'webp');
+
+module.exports = config;
